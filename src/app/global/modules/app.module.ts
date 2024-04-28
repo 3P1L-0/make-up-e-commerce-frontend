420c37a7b89ee543a7abc7fef7cae3adf4eed0ab
Piedoso Pedro <piedosopedro@gmail.com>
2024-04-28 22:37:04 +0100

    added a global dialog and toast component

--- src/app/global/modules/app.module.ts
+++ src/app/global/modules/app.module.ts
@@ -1,13 +1,14 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './routing.module';
-import { AppComponent } from '../app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './routing.module';
+import {AppComponent} from '../app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 import {HashLocationStrategy, Location, LocationStrategy, registerLocaleData} from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { AppGlobalModule } from './global.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {AppGlobalModule} from './global.module';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {AppPrimeNgModule} from "./primeng.module";
 
 registerLocaleData(localePt, 'pt');
 
@@ -20,11 +21,11 @@ registerLocaleData(localePt, 'pt');
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+    FontAwesomeModule,
+    AppPrimeNgModule,
     AppGlobalModule,
-    FontAwesomeModule
   ],
   providers: [
-    // {provide: ErrorStateMatcher, useClass: CustomErrorStateMatcher},
     {provide: LOCALE_ID, useValue: 'pt'},
     Location,
     {provide: LocationStrategy, useClass: HashLocationStrategy},

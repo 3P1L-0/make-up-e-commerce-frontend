046d9688a65dc8aa9d4196885364b27df471d59c
Piedoso Pedro <piedosopedro@gmail.com>
2024-05-12 22:44:17 +0100

    private main menu tweaked

--- src/app/private/components/app-shell/app-shell.module.ts
+++ src/app/private/components/app-shell/app-shell.module.ts
@@ -4,7 +4,7 @@ import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {AppHeaderModule} from "./header/header.module";
 import {AppMainMenuModule} from "./main-menu/main-menu.module";
-import {AppFooterModule} from "../../../global/components/layout/footer/footer.module";
+import {AppFooterModule} from "../../../global/components/footer/footer.module";
 
 @NgModule({
   declarations: [AppShellComponent],

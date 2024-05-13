e537b24d09d384715477926dc8691974ee92b0f4
Piedoso Pedro <piedosopedro@gmail.com>
2024-05-13 14:44:07 +0100

    partially implemented the landing page

--- src/app/public/components/landing-page/landing-page.component.ts
+++ src/app/public/components/landing-page/landing-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from "@angular/core";
+import {Component, inject} from "@angular/core";
+import {take} from "rxjs";
+import {Product} from "../../../global/model/cart/Product";
+import {ProductVariantDTO} from "../../../global/model/cart/dto/ProductVariantDTO";
+import {ServiceDTO} from "../../../global/model/cart/dto/ServiceDTO";
+import {PUBLIC_ROUTES} from "../../../global/configs";
+import {AppProductService} from "../../../private/services/product.service";
+import {AppServiceService} from "../../../private/services/services.service";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   host: {'class': 'app-landing-page-module'}
 })
-export class AppLandingPageComponent {}
+export class AppLandingPageComponent {
+  /* DEPENDENCIES */
+  private readonly _productsService = inject(AppProductService);
+  private readonly _servicesService = inject(AppServiceService);
+
+  /* MEMBERS */
+  public produtos: ProductVariantDTO[];
+  public servicos: ServiceDTO[];
+  public readonly routes = PUBLIC_ROUTES;
+
+  constructor() {
+    this._productsService.fetchVariants().pipe(take(4)).subscribe(items => { this.produtos = items });
+    this._servicesService.fetch().pipe(take(4)).subscribe(items => { this.servicos = items });
+  }
+}

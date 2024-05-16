8270721c1b5031baa7ff795d0aa0898900b4d35f
Mariano JavaSwing <marianogoncalves8@gmail.com>
2024-05-16 22:02:03 +0100

    order.ts file 'routes' has been created

---
+++ src/app/global/configs/routes/api/order.ts
@@ -0,0 +1,9 @@
+export const enum ORDER_API {
+  getById = 'orders/fetchBy/orderId/',
+  fetchByState = 'orders/fetchBy/state/',
+  fetchAll = 'orders/fetch/all',
+  completeOrder = 'orders/complete',
+  shipeOrder = 'orders/fetch/all',
+  cancelOrder = 'orders/complete'
+
+}

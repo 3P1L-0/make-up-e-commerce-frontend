2a9b1152d0b2a502cfd5c5ecb37e8c04f8ecd054
Mariano JavaSwing <marianogoncalves8@gmail.com>
2024-05-16 13:54:37 +0100

    PAYMENT_METHOD_API has been modified. deleteList path has been created

--- src/app/global/configs/routes/api/payment-method.ts
+++ src/app/global/configs/routes/api/payment-method.ts
@@ -2,6 +2,7 @@ export const enum PAYMENT_METHOD_API {
   fetch = "paymentMethods/fetch/all",
   getById = "paymentMethods/fetchBy/id/",
   deleteById = "paymentMethods/deleteBy/id/",
+  deleteList = "paymentMethods/delete/list",
   create = "paymentMethods/new",
   createList = "paymentMethods/new/list",
   update = "paymentMethods/update"
